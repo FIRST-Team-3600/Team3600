dab0583b9362253fb174510fd7affbba25945a3d
Music-Theory <protomith@gmail.com>
2015-01-24 09:54:12 -0500

    LiftSystem
    Classes for the lift system and testing it.

--- src/org/usfirst/frc/team3600/robot/subsystems/LiftSystem.java
+++ src/org/usfirst/frc/team3600/robot/subsystems/LiftSystem.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team3600.robot.subsystems;
 
 import org.usfirst.frc.team3600.robot.RobotMap;
 
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -11,12 +12,18 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class LiftSystem extends Subsystem {
     
-    Talon motor1 = new Talon(RobotMap.LIFT_ONE);
-    Talon motor2 = new Talon(RobotMap.LIFT_TWO);
+    CANTalon motor1 = new CANTalon(RobotMap.LIFT_ONE);
+    CANTalon motor2 = new CANTalon(RobotMap.LIFT_TWO);
     
     public void initDefaultCommand() {
+    	motor1.changeControlMode(CANTalon.ControlMode.Position);
+    	motor2.changeControlMode(CANTalon.ControlMode.Follower);
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void test() {
+    	motor1.setPosition(10);
     }
     
 }

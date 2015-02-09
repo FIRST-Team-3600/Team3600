80eef6d8145d10f24e3db76ecdb248ab65e16694
Music-Theory <protomith@gmail.com>
2015-02-09 18:34:10 -0500

    Changed Drive System
    The mechanical and electrical teams changed the motor system so that it
    would actually do the things it's supposed to, so I changed the drive
    command to take straight numbers from the joystick instead of taking
    them and inverting them because it doesn't need to do that anymore.

--- src/org/usfirst/frc/team3600/robot/commands/DriveCommand.java
+++ src/org/usfirst/frc/team3600/robot/commands/DriveCommand.java
@@ -38,14 +38,15 @@ public class DriveCommand extends Command {
     double r;
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	double x = stick.getRawAxis(0);
-    	double y = -stick.getRawAxis(1);
-    	double r = -stick.getRawAxis(4);
+    	double x = -stick.getRawAxis(0);
+    	double y = stick.getRawAxis(1);
+    	double r = stick.getRawAxis(4);
     	
     	x = Math.abs(x) < deadzoneX ? 0 : x;
     	y = Math.abs(y) < deadzoneY ? 0 : y;
     	r = Math.abs(r) < deadzoneR ? 0 : r;
     	
+    	//System.out.println(x + y + r);
     	Robot.DRIVE_SYSTEM.drive(x, r, y);
     }
 
